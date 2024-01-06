fdb596306092f976ae8503ff6f37ef4de95910c5
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-06 21:52:22 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
 import { favouriteBook, BookList, Card } from "./Product";
 
-
+const alertStyles = {
+  margin: 8,
+  padding: "12px 16px",
+  borderRadius: 4,
+  backgroundColor: "gray",
+  color: "white",
+};
 
 export default function App() {
   return (
@@ -15,7 +21,9 @@ export default function App() {
         <h1>Card title</h1>
 
       </Card>
-
+      <p style={alertStyles}>Please update your email!</p>
+      <p style={alertStyles}>There was an error during transaction!</p>
+      <p style={alertStyles}>Payment received, thank you for your purchase!</p>
     </>
   );
 }
