48a5e9359e1f474613eb08d021b3d13381d79772
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-06 18:33:42 +0100

    konsp

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,23 @@
-export const Product = () => {
-    return (
-      <div>
-        <h2>Tacos</h2>
-              <p>Price: 999 credits</p>
-      </div>
-    );
-  };
+
+
+// export const Product = props => {
+//   return (
+//     <div>
+//       <h2>{props.name}</h2>
+// 			<img src={props.imgUrl} alt={props.name} width="480" />
+// 			<p>Price: {props.price} credits</p>
+//     </div>
+//   );
+// };
+
+export const Product = ({
+  name,
+  imgUrl = "https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder",
+  price,
+}) => (
+  <div>
+		<h2>{name}</h2>
+    <img src={imgUrl} alt={name} width="480" />
+    <p>Price: {price} credits</p>
+  </div>
+);
