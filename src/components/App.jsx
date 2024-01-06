045fea3924b29b72ec72b4c7bdacaf0d8d999a90
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-07 00:09:46 +0100

    1

---
+++ src/components/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+import { favouriteBook, BookList, Card } from "./Product";
+import { Alert } from "./Alert";
+
+
+export default function App() {
+  return (
+    <>
+      <h1>Books of the week</h1>
+      
+      <BookList books={favouriteBook} />
+
+      <Card>
+      
+        <h1>Card title</h1>
+
+      </Card>
+      <Alert variant="info">
+        Would you like to browse our recommended products?
+      </Alert>
+      <Alert variant="error">
+        There was an error during your last transaction
+      </Alert>
+      <Alert variant="success">
+        Payment received, thank you for your purchase
+      </Alert>
+      <Alert variant="warning">
+        Please update your profile contact information
+      </Alert>
+
+
+      
+    </>
+  );
+}
