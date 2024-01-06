5350041b236fc96a041f804f5a7843f299a84724
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-06 19:40:59 +0100

    1

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,23 +1,21 @@
+export const favouriteBooks = [
+  { id: "id-1", name: "JS for beginners" },
+  { id: "id-2", name: "React basics" },
+  { id: "id-3", name: "React Router overview" },
+];
+
+export const BookList = ({ books }) => {
+  return (
+		<ul>
+			{books.map((book) => {
+			  return <li key={book.id}>{book.name}</li>;
+			})}
+		</ul>
+  );
+};
 
 
-// export const Product = props => {
-//   return (
-//     <div>
-//       <h2>{props.name}</h2>
-// 			<img src={props.imgUrl} alt={props.name} width="480" />
-// 			<p>Price: {props.price} credits</p>
-//     </div>
-//   );
-// };
 
-export const Product = ({
-  name,
-  imgUrl = "https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder",
-  price,
-}) => (
-  <div>
-		<h2>{name}</h2>
-    <img src={imgUrl} alt={name} width="480" />
-    <p>Price: {price} credits</p>
-  </div>
-);
+export const Card = ({ children }) => {
+  return <div>{children}</div>;
+};
