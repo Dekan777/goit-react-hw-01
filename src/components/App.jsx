c7678b6e5ccf55036c836372c19da1a166b621b2
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-07 21:07:50 +0100

    del

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,22 @@
 import "./App.css";
-// import { favouriteBook, BookList, Card } from "./Product";
-// import { Alert } from "./Alert";
 import { Profile } from "./Profile/Profile";
+import userData from "../userData.json";
+console.log(userData);
+console.log(userData.avatar);
 
 
 export default function App() {
   return (
     <>
-      <h1>Завдання 1 - Профіль соціальної мережі</h1>
-      <Profile/>
      
-
-      
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+     
     </>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import "modern-normalize";
 import App from './components/App'
-
-
 import './index.css'
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
