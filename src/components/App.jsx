f86600be48575d04580491846c19d5b767eebbed
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-08 00:43:51 +0100

    TransactionHistory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,8 @@ import { Profile } from "./Profile/Profile";
 import userData from "../userData.json";
 import friends from "../friends.json";
 import { FriendList } from "./FriendList/FriendList";
-
+import transactions from "../transactions.json";
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
 export default function App() {
   return (
     <>
@@ -15,9 +16,14 @@ export default function App() {
         stats={userData.stats}
       />
 
-      <>
-        <FriendList friends={friends} />
-      </>
+      <FriendList 
+       friends={friends} 
+      />
+
+      <TransactionHistory 
+       items={transactions} 
+      />
+
     </>
   );
 }
