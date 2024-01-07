f6ca1b2e88f73e6fd72d38d8de23347d7bc0ff38
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-07 01:00:58 +0100

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,14 @@
 import "./App.css";
-import { favouriteBook, BookList, Card } from "./Product";
-import { Alert } from "./Alert";
+// import { favouriteBook, BookList, Card } from "./Product";
+// import { Alert } from "./Alert";
 
 
 export default function App() {
   return (
     <>
-      <h1>Books of the week</h1>
+      <h1>Завдання 1 - Профіль соціальної мережі</h1>
       
-      <BookList books={favouriteBook} />
-
-      <Card>
-      
-        <h1>Card title</h1>
-
-      </Card>
-      <Alert variant="info">
-        Would you like to browse our recommended products?
-      </Alert>
-      <Alert variant="error">
-        There was an error during your last transaction
-      </Alert>
-      <Alert variant="success">
-        Payment received, thank you for your purchase
-      </Alert>
-      <Alert variant="warning">
-        Please update your profile contact information
-      </Alert>
-
+     
 
       
     </>
