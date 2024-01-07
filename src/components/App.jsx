6a92542c749f759f3bf26201bf4282a0a2d9ff05
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-07 01:07:36 +0100

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 // import { favouriteBook, BookList, Card } from "./Product";
 // import { Alert } from "./Alert";
+import { Profile } from "./Profile/Profile";
 
 
 export default function App() {
   return (
     <>
       <h1>Завдання 1 - Профіль соціальної мережі</h1>
-      
+      <Profile/>
      
 
       
