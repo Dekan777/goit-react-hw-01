a038b82a80edfce63383d31811370d36d539b86b
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-08 08:57:54 +0100

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -2,6 +2,7 @@ import css from "./TransactionHistory.module.css";
 
 export const TransactionHistory = ({ items }) => {
   return (
+    
     <table className={css.transactionHistory}>
       <thead>
         <tr>
