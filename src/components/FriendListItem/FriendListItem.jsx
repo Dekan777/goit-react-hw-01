c481b4c999e7556692722652628df2bf5c2ec00c
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-08 17:28:17 +0100

    end css

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -3,11 +3,9 @@ import css from "./FriendListItem.module.css";
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <div className={css.item}>
-      <img className={css.avatar} src={avatar} alt="Avatar" width="48" />
+      <img className={css.avatar} src={avatar} alt="Avatar" width="70" />
       <p className={css.name}>{name}</p>
-      <p
-        className={`${css.status} ${isOnline ? css.online : css.offline}`}
-      >
+      <p className={`${css.status} ${isOnline ? css.online : css.offline}`}>
         {isOnline ? "Online" : "Offline"}
       </p>
     </div>
