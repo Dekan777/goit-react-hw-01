a2d02f192391b59e892b85db9103dbbc63e5b3ed
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-13 00:19:23 +0100

    composes

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,12 +1,12 @@
 import css from './FriendListItem.module.css';
-import clsx from 'clsx';
+// import clsx from 'clsx';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
-  const statusClass = clsx(css.status, {
-    [css.online]: isOnline,
-    [css.offline]: !isOnline,
-  });
-
+  // const statusClass = clsx(css.status, {
+  //   [css.online]: isOnline,
+  //   [css.offline]: !isOnline,
+  // });
+  const statusClass = isOnline ? css.online : css.offline;
   return (
     <div className={css.item}>
       <img className={css.avatar} src={avatar} alt="Avatar" width="80" />
